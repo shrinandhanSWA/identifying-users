6be0a44bc811542d723413fe13b24a854dbe5518
Nandhu <shrindhancr@gmail.com>
2023-03-29 16:26:13 +0100

    ADD code to analyze different activation functions + save + print results

--- analyis.py
+++ analyis.py
@@ -4,111 +4,141 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+from process_data import process_data
+from rf_classifier import classifier
 
-def hsu_day_distribution():
-    input = pd.read_csv('csv_files/EveryonesAppData.csv')
 
-    day_spreads = [[] for _ in range(7)]
-    curr_user = None
-    curr_user_days_seen = []
-    day_count = 0
+# verifying that the ofcom dataset really has 5064 people + plotting how much info there is
+def ofcom_data_analysis():
+
+    input = pd.read_csv('csv_files/ofcom_processed.csv')
+
+    users_to_days = {}
+
+    # iterate through every input
+    for row in input.itertuples():
+        user = row.participantnumber
+        timestamp = row.timestamp
+        event_time = datetime.fromtimestamp(int(timestamp))
+        event_day = event_time.day
+
+        if user not in users_to_days:
+            users_to_days[user] = set()
+
+        users_to_days[user].add(event_day)
+
+    print(f'There were {len(users_to_days)} users')
+
+    # count number of days
+    for user, days in users_to_days.items():
+        day_count = len(days)
+        users_to_days[user] = day_count
+
+    days = list(users_to_days.values())
+
+    plt.hist(days)
+    plt.xlabel("Days of data")
+    plt.ylabel("Number of users")
+    plt.title("Distribution of days of data for each user")
+    plt.show()
+
+    users_enough_data = len(list(filter(lambda x: x > 6, days)))
+    print(f'There were {users_enough_data} users with 7 or more days of data')
+
+
+def hsu_app_analysis():
+    input = pd.read_csv('csv_files/HSU_processed.csv', index_col=[0, 1])
+
+    cols = list(input.columns)
+    cols = [c.split('-')[0] for c in cols]
+    apps_count = {c:0 for c in cols}
 
     for row in input.itertuples():
+        # go through row
+        for i, app_usage in enumerate(list(row)[1:]):
+            apps_count[cols[i]] += app_usage
 
-        if row.participantnumber != curr_user:
-            curr_user = row.participantnumber
-            day_count = 1
-            curr_user_days_seen = []
+    sorted_durations = sorted(apps_count.items(), key=lambda x: x[1], reverse=True)
 
+    bad_apps = ['System UI', 'Samsung Experience Home', 'TouchWiz Home', 'Xperia Home',
+               'Android System', 'x']  # To be excluded from popular apps calculation]
 
-        timestamp = row.timestamp
-        timestamp_datetime = datetime.fromtimestamp(int(timestamp))
-        actual_day = timestamp_datetime.day
-        day_of_week = datetime.weekday(timestamp_datetime)
-        # day_of_week - 0 --> Monday, ... , 6 --> Sunday
+    top_apps = [x for x, _ in sorted_durations if x not in bad_apps][:19]
 
-        # Days have multiple entries, so need to consider only first entry of the day
-        if actual_day in curr_user_days_seen:
-            continue
-
-        # only want to consider the first 7 days
-        if day_count > 7:
-            continue
-
-        curr_user_days_seen.append(actual_day)
-        day_spreads[day_count - 1].append(day_of_week)
-        day_count += 1
-
-    # ------------------------------------
-
-    input = pd.read_csv('csv_files/EveryonesAppData.csv')
-
-    weekday_day_spreads = [[] for _ in range(7)]
-    curr_user = None
-    curr_user_days_seen = []
-    day_count = 0
-
-    for row in input.itertuples():
-
-        if row.participantnumber != curr_user:
-            curr_user = row.participantnumber
-            day_count = 1
-            curr_user_days_seen = []
-
-        timestamp = row.timestamp
-        timestamp_datetime = datetime.fromtimestamp(int(timestamp))
-        actual_day = timestamp_datetime.day
-        day_of_week = datetime.weekday(timestamp_datetime)
-        # day_of_week - 0 --> Monday, ... , 6 --> Sunday
-
-        # Days have multiple entries, so need to consider only first entry of the day
-        if actual_day in curr_user_days_seen:
-            continue
-
-        # only want to consider the first 7 days
-        if day_count > 7:
-            continue
-
-        # ignore weekends
-        if day_of_week in [5, 6]:
-            continue
-
-        curr_user_days_seen.append(actual_day)
-        weekday_day_spreads[day_count - 1].append(day_of_week)
-        day_count += 1
+    print(top_apps)
 
 
-    fig, axs = plt.subplots(3, 3)
-    axs[0, 0].hist(day_spreads[0], label='all days')
-    axs[0, 0].hist(weekday_day_spreads[0], label='weekdays')
-    axs[0, 0].legend(prop={'size': 5})
-    axs[0, 0].set_title('Day1', size=10)
-    axs[0, 1].hist(day_spreads[1], label='all days')
-    axs[0, 1].hist(weekday_day_spreads[1], label='weekdays')
-    axs[0, 1].legend(prop={'size': 5})
-    axs[0, 1].set_title('Day2', size=10)
-    axs[0, 2].hist(day_spreads[2], label='all days')
-    axs[0, 2].hist(weekday_day_spreads[2], label='weekdays')
-    axs[0, 2].legend(prop={'size': 5})
-    axs[0, 2].set_title('Day3', size=10)
-    axs[1, 0].hist(day_spreads[3], label='all days')
-    axs[1, 0].hist(weekday_day_spreads[3], label='weekdays')
-    axs[1, 0].legend(prop={'size': 5})
-    axs[1, 0].set_title('Day4', size=10)
-    axs[1, 1].hist(day_spreads[4], label='all days')
-    axs[1, 1].hist(weekday_day_spreads[4], label='weekdays')
-    axs[1, 1].legend(prop={'size': 5})
-    axs[1, 1].set_title('Day5', size=10)
-    axs[1, 2].hist(day_spreads[5], label='all days')
-    axs[1, 2].hist(weekday_day_spreads[5], label='weekdays')
-    axs[1, 2].legend(prop={'size': 5})
-    axs[1, 2].set_title('Day6', size=10)
-    axs[2, 0].hist(day_spreads[6], label='all days')
-    axs[2, 0].hist(weekday_day_spreads[6], label='weekdays')
-    axs[2, 0].legend(prop={'size': 5})
-    axs[2, 0].set_title('Day7', size=10)
+def time_gran_analysis(name):
+
+    files_to_test = {}
+    time_bin_list = [60, 180, 360, 720, 1440]
+
+    for time_bins in time_bin_list:
+
+        # analysis that investigates different activations/thresholds
+        # stores results in results.txt, will all be plotted later
+        input_file = 'csv_files/ofcom_processed.csv'
+        output_file = f'csv_files/AllDays_{time_bins // 60}h.csv'
+        pop_apps = []
+        weekdays_split = False
+        z_scores = False
+        day_lim = 7  # limit each person to only day_lim days of data
+        user_lim = 778  # limit number of users, default: 10000 (i.e. no limit)
+        process_data(input_file, output_file, time_bins, pop_apps, weekdays_split, z_scores, day_lim, user_lim=user_lim)
+        files_to_test[f'{time_bins // 60}h'] = output_file
+
+    # once all the data has been generated, call the classifier with these files
+    names, accs, sems = classifier(files_to_test)
+
+    # write to results.txt - to be loaded up later
+    with open('results.txt', 'a') as f:
+        to_write = [name, ';']
+        for acc in accs:
+            to_write.append(str(acc))
+            to_write.append(',')
+        to_write = to_write[:-1]
+        to_write.append(';')
+        for sem in sems:
+            to_write.append(str(sem))
+            to_write.append(',')
+        to_write = to_write[:-1]
+        to_write.append('\n')
+
+        f.write(''.join(to_write))
+
+    return
+
+
+# helper to plot the results in results.txt
+def plot_results():
+    with open('results.txt', 'r') as f:
+        lines = f.readlines()
+
+        # to make the printing a bit clearer
+        targ_indices = [0, 1, 2]
+        lines = [lines[i] for i in targ_indices]
+
+        for line in lines:
+
+            line = line.strip()
+            line_split = line.split(';')
+            name = line_split[0]
+            labels = ['1', '3', '6', '12', '24']
+            means = [float(x) for x in line_split[1].split(',')]
+            sems = [float(x) for x in line_split[2].split(',')]
+
+            plt.errorbar(x=labels, y=means, yerr=sems, capsize=10, fmt='o', label=f'{name}')
+
+    plt.xlabel('time bins (h)')
+    plt.ylabel('accuracy')
+    plt.legend()
     plt.show()
 
 
 if __name__ == "__main__":
-    hsu_day_distribution()
+    # ofcom_data_analysis()
+    # hsu_app_analysis()
+    # time_gran_analysis('tanh')
+    plot_results()
+
+    print()
