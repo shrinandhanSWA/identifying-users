a98109f4fe0c3e4e64b24126df08e4008c6aa223
Nandhu <shrindhancr@gmail.com>
2023-04-11 12:46:02 +0100

    ADD extra info function call to ofcom file + comparison in rf_classifier

--- ofcom.py
+++ ofcom.py
@@ -4,7 +4,7 @@
 import pandas as pd
 from dateutil.parser import parse
 from tqdm import tqdm
-from process_data import process_data
+from process_data import process_data, extra_data
 
 
 # Simple csv crop function
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     # set up arguments, then call the function
     input_file = 'csv_files/ofcom_processed.csv'
 
-    output_file = 'csv_files/AllDays.csv'  # needs to be a tuple when weekdays_split is True
+    output_file = 'csv_files/AllDaysExtra.csv'  # needs to be a tuple when weekdays_split is True
     # output_file = ('csv_files/Weekdays.csv', 'csv_files/Weekends.csv')
 
     time_bins = 1440  # in minutes
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 
     weekdays_split = False
 
-    z_scores = True
+    z_scores = False
 
     day_lim = 7  # limit each person to only day_lim days of data
 
@@ -84,4 +84,9 @@ if __name__ == '__main__':
 
     selected_days = ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7']
 
-    process_data(input_file, output_file, time_bins, pop_apps, weekdays_split, z_scores, day_lim)
+    agg = True
+
+    process_data(input_file, output_file, time_bins, pop_apps, weekdays_split, z_scores, day_lim, user_lim=user_lim,
+                 agg=agg)
+
+    extra_data(input_file, output_file, 360, weekdays_split, z_scores, day_lim, user_lim=user_lim, agg=agg)
--- rf_classifier.py
+++ rf_classifier.py
@@ -6,13 +6,105 @@ from statistics import mean
 import matplotlib.pyplot as plt
 
 
-# weekday classifier
-def classifier(files_to_test):
-
+# weekday classifier - for csv files with non-aggregated days
+def all_data_classifier(files_to_test):
     names = list(files_to_test.keys())
     avg_accs = []
     avg_sems = []
 
+    # do analysis for each given file
+    for f_name, f_path in files_to_test.items():
+        # load data from CSV
+        print('loading data...')
+        input_df = pd.read_csv(f_path)
+
+        # Calculate number of users
+        users = list(set([row.Person for row in input_df.itertuples()]))
+
+        # for each user, generate a list (for their accuracies across each day in test_days)
+        users_data = {}
+        for user in users:
+            users_data[user] = []
+
+        # Calculate the days - slightly different than for aggregated data
+        test_days = sorted(list(set([row.Day.split('-')[0] for row in input_df.itertuples()])))
+
+        # Run analysis across each day in test_days
+        for test_day in test_days:
+
+            # split data into training and test set
+            training_set = []
+            test_set = []
+            training_labels = []
+            test_labels = []
+
+            # iterate through the processed data, to construct the sets
+            for row in input_df.itertuples():
+
+                row = list(row)
+
+                person = row[1]
+                day = row[2].split('-')[0]
+                data = row[3:]
+
+                if day == test_day:
+                    test_labels.append(person)
+                    test_set.append(data)
+                else:
+                    training_labels.append(person)
+                    training_set.append(data)
+
+            # convert everything to numpy arrays
+            training_set = np.array(training_set)
+            training_labels = np.array(training_labels)
+            test_set = np.array(test_set)
+            test_labels = np.array(test_labels)
+
+            # Shuffling training data
+            seed = 1843  # np.random.randint(0, 10000)
+            np.random.seed(seed)
+            np.random.shuffle(training_set)
+            np.random.seed(seed)
+            np.random.shuffle(training_labels)
+
+            rf = RandomForestClassifier(n_estimators=100, random_state=46)
+
+            rf.fit(training_set, training_labels)
+
+            predictions = rf.predict(test_set)
+
+            for i, user in enumerate(test_labels):
+                users_data[user].append(1 if user == predictions[i] else 0)
+
+            N = test_labels.shape[0]
+            accuracy = (test_labels == predictions).sum() / N
+
+            print(f'accuracy when the test day is {test_day}: {accuracy * 100}%')
+
+            # try sigmoid/other activation functions for the thresholding
+
+        avg_acc = mean(mean(data) for _, data in users_data.items())
+        avg_sem = mean(sem(data) for _, data in users_data.items())
+
+        avg_accs.append(avg_acc)
+        avg_sems.append(avg_sem)
+
+        print(f'done analyzing {f_name}')
+
+    # plot accs and sem for this analysis
+    plt.errorbar(x=names, y=avg_accs, yerr=avg_sems, capsize=10, fmt='o', label='default(mean)', c='g')
+
+    plt.show()
+
+    # return some information if the caller wishes to use it
+    return names, avg_accs, avg_sems
+
+
+# weekday classifier - for csv files with aggregated days
+def agg_classifier(files_to_test):
+    names = list(files_to_test.keys())
+    avg_accs = []
+    avg_sems = []
 
     # do analysis for each given file
     for f_name, f_path in files_to_test.items():
@@ -56,7 +148,6 @@ def classifier(files_to_test):
                     training_labels.append(person)
                     training_set.append(data)
 
-
             # convert everything to numpy arrays
             training_set = np.array(training_set)
             training_labels = np.array(training_labels)
@@ -97,7 +188,6 @@ def classifier(files_to_test):
     # plot accs and sem for this analysis
     plt.errorbar(x=names, y=avg_accs, yerr=avg_sems, capsize=10, fmt='o', label='default(mean)', c='g')
 
-
     plt.show()
 
     # return some information if the caller wishes to use it
@@ -105,7 +195,7 @@ def classifier(files_to_test):
 
 
 if __name__ == '__main__':
+    files_to_test = {'OFCOM_APP_OVERALL_USAGE': 'csv_files/AllDaysExtra.csv',
+                     'OFCOM_APP_USAGE': 'csv_files/AllDaysOriginal.csv'}
 
-    files_to_test = {'OFCOM': 'csv_files/AllDays.csv', 'OFCOM_WEEKDAY_ONLY': 'csv_files/Weekdays.csv'}
-
-    classifier(files_to_test)
+    all_data_classifier(files_to_test)
