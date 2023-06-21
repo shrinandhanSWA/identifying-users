a4c0eb4089723fc3df5839c945fc8960b82f52d7
Nandhu <shrindhancr@gmail.com>
2023-06-21 19:30:17 +0100

    CRAWDAD.py cleanup

--- crawdad.py
+++ crawdad.py
@@ -1,9 +1,19 @@
 import os
 import pandas as pd
-from tqdm import tqdm
+from tqdm import tqdm, trange
+
+from analyis import check_fingerprint_uniqueness, check_hamming_distances, dual_line_plot
+from merge_datasets import compute_N_pop_apps
+from process_data import DurationsPerApp, PickupsPerApp, DurationsOverall, PickupsOverall, Fingerprint, PickupsAppClass, \
+    AverageDurationPerApp, DurationsAppClass, FirstUseTime, LastUseTimeApp, MeanDurationBetweenPickups, LastUseTime
+from rf_classifier import agg_classifier, line_plot
+import random
 
 INPUT = 'csv_files/mobile_phone_use/data'
 OUTPUT = 'csv_files/mobile_phone_use/processed'
+POP_APPS = tuple(['Settings', 'WhatsApp', 'Internet', 'Facebook', 'YouTube', 'Twitter', 'Google Search', 'Camera',
+                  'Instagram', 'Clock', 'Candy Crush Saga', 'Photos', 'Farm Heroes Saga', 'Jade Monkey', 'Phone',
+                  'Gmail', 'Contacts', 'Clash Royale'])
 
 
 # process one user - each user has a CSV file in INPUT
@@ -115,14 +125,23 @@ def merge_csvs():
 
 # do some pre-processing - CSV is already processed, only thing to do is rename the participant numbers (like ofcom.py)
 def process_merged_csv(path):
-
     input = pd.read_csv(path)
 
     users_seen = {}
     users_count = 1
 
-    # Pro-processing required for Ofcom dataset - sort the data!
+    # Pre-processing required for Ofcom dataset - sort the data!
     # Before that, need to convert UUIDs + timestamps to integers, to be sorted
+    # Also do app renaming as pet apps.txt
+
+    # read in apps.txt --> app name dict
+    apps_dict = {}
+    with open('csv_files/mobile_phone_use/apps.txt', 'r') as file:
+        lines = [line.rstrip() for line in file]
+        for line in lines:
+            stuff = line.split(':')
+            apps_dict[stuff[0]] = stuff[1]
+
     # iterate through all the inputs
     for row in tqdm(input.itertuples(), total=len(input.index)):
         user = row.participantnumber
@@ -131,9 +150,16 @@ def process_merged_csv(path):
             users_count += 1
         input.at[row.Index, 'participantnumber'] = users_seen[user]
 
-        timestamp = row.timestamp
-        unix_time = int(timestamp) // 1000
-        input.at[row.Index, 'timestamp'] = unix_time
+        old_app_name = row.event
+        new_app_name = old_app_name
+
+        if old_app_name in apps_dict:
+            new_app_name = apps_dict[old_app_name]
+
+        input.at[row.Index, 'event'] = new_app_name
+
+        timestamp = int(row.timestamp)
+        input.at[row.Index, 'timestamp'] = timestamp
 
     print('sorting...')
     input = input.sort_values(['participantnumber', 'timestamp'])
@@ -144,46 +170,59 @@ def process_merged_csv(path):
     input.to_csv('csv_files/crawdad_processed.csv', index=False)
 
 
+# process data
+def process(output_file='csv_files/crawdad_pop_apps.csv', pop_apps=POP_APPS, verbose=False):
+    # set up arguments, then call the function
+    input_file = 'csv_files/crawdad_processed.csv'
+
+    time_bins = 1440  # in minutes
+
+    weekdays_split = False
+
+    z_scores = False
+
+    day_lim = 7  # limit each person to only day_lim days of data
+
+    user_lim = 10000  # limit number of users, default: 10000 (i.e. no limit)
+
+    agg = True
+
+    # remember, caller has to clear output_file
+    f = open(output_file, "w+")
+    f.close()
+
+    # targeted app set - CRAWDAD
+    targ_apps = ['WhatsApp', 'Internet', 'Settings', 'Google Search', 'Contacts', 'Photos', 'Phone', 'Facebook',
+                 'Clock', 'Gmail', 'Twitter', 'Instagram', 'Camera', 'YouTube']
+
+    # Do the analysis - different metrics left here
+    # PickupsPerApp(input_file, output_file, 1440, targ_apps, weekdays_split, z_scores, day_lim, user_lim=user_lim,
+    #               agg=agg, verbose=verbose).process_data()
+    # DurationsPerApp(input_file, output_file, 1440, targ_apps, weekdays_split, z_scores, day_lim, user_lim=user_lim,
+    #                 agg=agg, verbose=verbose).process_data()
+    # PickupsAppClass(input_file, output_file, 1440, [], weekdays_split, z_scores, day_lim, user_lim=user_lim,
+    #                 agg=agg, verbose=verbose).process_data()
+    # DurationsAppClass(input_file, output_file, 1440, [], weekdays_split, z_scores, day_lim, user_lim=user_lim,
+    #                 agg=agg, verbose=verbose).process_data()
+    # DurationsOverall(input_file, output_file, 1440, [], weekdays_split, z_scores, day_lim, user_lim=user_lim,
+    #                 agg=agg, verbose=verbose).process_data()
+    # LastUseTime(input_file, output_file, 1440, [], weekdays_split, z_scores, day_lim, user_lim=user_lim,
+    #                 agg=agg, verbose=verbose).process_data()
+    # FirstUseTime(input_file, output_file, 1440, [], weekdays_split, z_scores, day_lim, user_lim=user_lim,
+    #                 agg=agg, verbose=verbose).process_data()
+    # LastUseTimeApp(input_file, output_file, 1440, [], weekdays_split, z_scores, day_lim, user_lim=user_lim,
+    #               agg=agg, verbose=verbose).process_data()
+    Fingerprint(input_file, output_file, 1440, pop_apps, weekdays_split, z_scores, day_lim, user_lim=user_lim,
+                  agg=agg).process_data()
+
+
 if __name__ == '__main__':
-    process_all_users(INPUT, OUTPUT)
+
+    # functions to generate code
+    # process_all_users(INPUT, OUTPUT)
     # merge_csvs()
     # process_merged_csv('csv_files/crawdad_raw.csv')
 
-    # OFCOM only has 18 apps
-    # compute_N_pop_apps(18, 'csv_files/crawdad_pop_apps.csv')
 
-
-    # set up arguments, then call the function
-    # input_file = 'csv_files/crawdad_processed.csv'
-    #
-    # output_file = 'csv_files/crawdad_pop_apps.csv'  # needs to be a tuple when weekdays_split is True
-    # # output_file = ('csv_files/ofcom_10_weekdays.csv', 'csv_files/ofcom_10_weekends.csv')
-    #
-    # time_bins = 1440  # in minutes
-    #
-    # pop_apps = []
-    #
-    # weekdays_split = False
-    #
-    # z_scores = False
-    #
-    # day_lim = 7  # limit each person to only day_lim days of data
-    #
-    # user_lim = 778  # limit number of users, default: 10000 (i.e. no limit)
-    #
-    # selected_days = ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7']
-    #
-    # agg = True
-    #
-    # # remember, caller has to clear output_file
-    # f = open(output_file, "w+")
-    # f.close()
-    #
-    # # f = open(output_file[0], "w+")
-    # # f.close()
-    # # f = open(output_file[1], "w+")
-    # # f.close()
-    #
-    # # Do the analysis
-    # DurationsPerApp(input_file, output_file, time_bins, pop_apps, weekdays_split, z_scores, day_lim, user_lim=user_lim,
-    #                 agg=agg).process_data()
+    process('csv_files/crawdad_1_week_test.csv')
+    # agg_classifier({'CRAWDAD': 'csv_files/crawdad_pop_apps.csv'}, n_trees=500)
